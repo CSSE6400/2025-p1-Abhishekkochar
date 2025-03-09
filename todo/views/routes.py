e3e34d82c30b6309b68a5b72a55cb3d645f9d2fc
Abhishekkochar <abhishekkochar2@gmail.com>
2025-03-10 01:03:30 +1000

    Attempt 2
    Signed-off-by: Abhishekkochar <abhishekkochar2@gmail.com>

--- todo/views/routes.py
+++ todo/views/routes.py
@@ -2,18 +2,6 @@ from flask import Blueprint, jsonify
 
 api = Blueprint('api', __name__, url_prefix='/api/v1')
 
-def test_data():
-    return jsonify([{
-        "id": 1,
-        "title": "Watch CSSE6400 Lecture",
-        "description": "Watch the CSSE6400 lecture on ECHO360 for week 1",
-        "completed": True,
-        "deadline_at": "2023-02-27T00:00:00",
-        "created_at": "2023-02-20T00:00:00",
-        "updated_at": "2023-02-20T00:00:00"
-    }])
-
-
 @api.route('/health')
 def health():
     return jsonify({"status": "ok"})
@@ -28,12 +16,20 @@ def get_todos():
         "deadline_at": "2023-02-27T00:00:00",
         "created_at": "2023-02-20T00:00:00",
         "updated_at": "2023-02-20T00:00:00"
-    }])
+    }.json])
 
 @api.route('/todos/<int:id>', methods=['GET'])
 def get_todo(id):
-    return test_data()
-    
+      return jsonify([{
+        "id": id,
+        "title": "Watch CSSE6400 Lecture",
+        "description": "Watch the CSSE6400 lecture on ECHO360 for week 1",
+        "completed": True,
+        "deadline_at": "2023-02-27T00:00:00",
+        "created_at": "2023-02-20T00:00:00",
+        "updated_at": "2023-02-20T00:00:00"
+    }])
+
 @api.route('/todos', methods=['POST'])
 def create_todo():
     return jsonify([{
